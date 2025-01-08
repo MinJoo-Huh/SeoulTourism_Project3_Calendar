e82abcb83352fff7b8df2202065766b834316e30
Minjoo Huh <huhminjoo@naver.com>
2025-01-08 11:29:39 +0900

    feat: location json dummy data test

--- test/src/App.js
+++ test/src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import CalendarTest from "./pages/CalendarTest";
 import TimeTableTest from "./pages/TimeTableTest";
+import LocationTest from "./pages/LocationTest";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<CalendarTest />} />
       <Route path="/timetable" element={<TimeTableTest />} />
+      <Route path="/location" element={<LocationTest />} />
     </Routes>
   );
 }
