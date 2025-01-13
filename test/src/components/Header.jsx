31db522207def5bd28aac6c637bdf608b7e47c73
Minjoo Huh <huhminjoo@naver.com>
2025-01-13 13:51:30 +0900

    feat: Header에서 language 값 전역 처리
    - Redux toolkit을 이용하여 language 전역 처리

--- test/src/components/Header.jsx
+++ test/src/components/Header.jsx
@@ -1,28 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router";
 import styled from "styled-components";
-
-const HeaderContainer = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
-`;
+import { setLanguage } from "../features/languageSlice";
 
 const Header = () => {
-  const [language, setLanguage] = useState("ko");
+  const dispatch = useDispatch();
+  const language = useSelector((state) => state.language.language); // state.action.state_name
 
   const handleLanguageChange = (e) => {
-    setLanguage(e.target.value);
+    dispatch(setLanguage(e.target.value));
   };
 
+  console.log(language);
   return (
     <>
       <HeaderContainer>
         <h2>This is Header</h2>
-        <select onChange={handleLanguageChange}>
-          <option value="ko">한국어</option>
+        <select value={language} onChange={handleLanguageChange}>
           <option value="en">English</option>
+          <option value="ko">한국어</option>
           <option value="ja">日本語</option>
           <option value="zh">中文</option>
         </select>
@@ -31,5 +28,12 @@ const Header = () => {
     </>
   );
 };
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+`;
 
 export default Header;
