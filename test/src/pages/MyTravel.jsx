f220c885e44692f1e624f13e1923486e009f0a2b
Minjoo Huh <huhminjoo@naver.com>
2025-01-17 01:32:37 +0900

    feat: MyTravel 페이지 styled-components로 CSS 꾸미기
    - 버튼 활성화에 따라, 배경 색상, 호버 시 배경 색상, 폰트 색상 지정.

--- test/src/pages/MyTravel.jsx
+++ test/src/pages/MyTravel.jsx
@@ -1,22 +1,69 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import InterestList from "../components/InterestList";
 import Itinerary from "../components/Itinerary";
 
+const Container = styled.div`
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  text-align: center;
+  margin-bottom: 40px;
+`;
+
+const Tabs = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+
+  p {
+    font-size: 20px;
+    margin: 0 40px;
+  }
+`;
+
+const TabButton = styled.button`
+  background: ${(props) => (props.active ? "gray" : "#fff")};
+  color: ${(props) => (props.active ? "#fff" : "#000")};
+  font-size: 1rem;
+  border: none;
+  padding: 10px 20px;
+  margin: 0 5px;
+  cursor: pointer;
+  border-radius: 5px;
+  &:hover {
+    background: ${(props) => (props.active ? "#555" : "#ddd")};
+  }
+`;
+
 const MyTravel = () => {
   const [activeTab, setActiveTab] = useState("interest");
+
   return (
-    <div>
-      <h1>나의 여행</h1>
-      <div>
-        <button onClick={() => setActiveTab("interest")}>관심 리스트</button>
+    <Container>
+      <Title>나의 여행</Title>
+      <Tabs>
+        <TabButton
+          active={activeTab === "interest"}
+          onClick={() => setActiveTab("interest")}
+        >
+          관심 리스트
+        </TabButton>
         <p>|</p>
-        <button onClick={() => setActiveTab("calendar")}>여행 일정</button>
-      </div>
+        <TabButton
+          active={activeTab === "calendar"}
+          onClick={() => setActiveTab("calendar")}
+        >
+          여행일정
+        </TabButton>
+      </Tabs>
       <div>
         {activeTab === "interest" && <InterestList />}
         {activeTab === "calendar" && <Itinerary />}
       </div>
-    </div>
+    </Container>
   );
 };
 
