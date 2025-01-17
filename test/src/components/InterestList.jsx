887641fe0552e4fd21e205256fa95e901ddaa73b
Minjoo Huh <huhminjoo@naver.com>
2025-01-17 10:31:31 +0900

    feat: 관심 여행지 styled-components로 CSS 꾸미기
    - background-color 위치 보기 위해 임시로 지정.

--- test/src/components/InterestList.jsx
+++ test/src/components/InterestList.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 20px;
+  background: #f9f9f9; // 임시
+  border-radius: 10px;
+`;
 
 const InterestList = () => {
   return (
-    <div>
-      <h2>관심 여행지</h2>
-    </div>
+    <Container>
+      {/* <h1>관심 리스트</h1> */}
+      <p>여기에 관심 리스트가 표시됩니다.</p>
+    </Container>
   );
 };
 
